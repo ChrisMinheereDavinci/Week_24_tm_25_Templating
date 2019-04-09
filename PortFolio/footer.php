440d943c9a1496074fc431543a9902da6b8a3b58
Chris_Minheere <99040522@mydavinci.nl>
2019-04-09 09:38:39 +0200

    update

--- PortFolio/footer.php
+++ PortFolio/footer.php
@@ -1,7 +1,15 @@
 <!-- Footer -->
 <footer class="py-5 bg-dark">
     <div class="container">
-      <p class="m-0 text-center text-white">Copyright &copy; Chris Minheere 2019</p>
+      <p class="m-0 text-center text-white">Copyright &copy; Chris Minheere 2019      <?php
+date_default_timezone_set("Europe/Amsterdam");
+$time = date("D-M-Y");
+echo "$time";
+?></p>
+
     </div>
+    
+
+
     <!-- /.container -->
   </footer>
